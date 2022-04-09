5e2fa0b63f2e431025f2fe9f2c71fbab1e5ec6de
6mile <info@6mile.dev>
2022-04-09 15:59:11 +0900

    fix minor changes

--- getTimeTable.py
+++ getTimeTable.py
@@ -4,6 +4,11 @@ from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 import re
+import json
+import sys
+
+args = sys.argv  # 引数を取得
+
 
 driver = webdriver.Chrome(
     executable_path='C:\chromedriver_win32\\chromedriver.exe')
@@ -15,7 +20,7 @@ driver.get("https://kyo-web.teu.ac.jp/campussy/")  # アクセスするURL
 driver.switch_to.frame(driver.find_element_by_name("search"))  # 検索フレームを切り替える
 
 Select(driver.find_element_by_id('jikanwariShozokuCode')
-       ).select_by_value('CS')  # コンピュータサイエンス学部
+       ).select_by_value(args[1])  # コンピュータサイエンス学部
 
 Select(driver.find_element_by_name('_displayCount')
        ).select_by_value('200')  # 一覧表示件数
@@ -60,37 +65,33 @@ p = getPageNum()  # 現在のページ数及び全体ページ数取得関数を
 aTagN = int(len(driver.find_elements_by_xpath(
     '/html/body/form/div[2]/p[1]/a'))) - 1
 
-driver.find_elements_by_xpath(
-    '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
-
-while p[0] < p[1]:  # 現在のページ数が全体ページ数より小さい間
-
-    p = getPageNum()  # 現在のページ数及び全体ページ数取得関数を実行
-
-    print(p[0])
-
-    driver.switch_to.default_content()  # 検索フレームをもとに戻す
-
-    driver.switch_to.frame(
-        driver.find_element_by_name("result"))  # 検索結果フレームを切り替える
-
-    thNum = len(driver.find_elements_by_xpath(
-        '/html/body/form/div[2]/table/tbody/tr'))  # エレメント数取得
-
-    print("エレメント " + str(thNum))
-
-    for num in range(1, thNum):
-        array.append(driver.find_element_by_xpath(
-            '/html/body/form/div[2]/table/tbody/tr[' + str(num) + ']/td[4]').text)
-
-    aTagN = len(driver.find_elements_by_xpath(
-        '/html/body/form/div[2]/p[1]/a')) - 1
-
+if(aTagN > 0):
     driver.find_elements_by_xpath(
         '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
 
+    while p[0] < p[1]:  # 現在のページ数が全体ページ数より小さい間
+
+        p = getPageNum()  # 現在のページ数及び全体ページ数取得関数を実行
+
+        driver.switch_to.default_content()  # 検索フレームをもとに戻す
+
+        driver.switch_to.frame(
+            driver.find_element_by_name("result"))  # 検索結果フレームを切り替える
+
+        thNum = len(driver.find_elements_by_xpath(
+            '/html/body/form/div[2]/table/tbody/tr'))  # エレメント数取得
+
+        for num in range(1, thNum):
+            array.append(driver.find_element_by_xpath(
+                '/html/body/form/div[2]/table/tbody/tr[' + str(num) + ']/td[4]').text)
+
+        aTagN = len(driver.find_elements_by_xpath(
+            '/html/body/form/div[2]/p[1]/a')) - 1
+
+        driver.find_elements_by_xpath(
+            '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
+
 driver.quit()
 
-print(array)
-
-print(len(array))
+with open('test.json', 'w') as f:
+    json.dump(array, f, ensure_ascii=False, indent=4)
