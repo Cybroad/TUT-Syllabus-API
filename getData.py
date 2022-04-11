4bcb28f4aeb459693b444a664dbb6d4d756b4460
6mile <info@6mile.dev>
2022-04-11 23:23:39 +0900

    情報取得方法の変更

--- getData.py
+++ getData.py
@@ -1,13 +1,13 @@
 from array import array
+from asyncio.windows_events import NULL
 import requests
 from bs4 import BeautifulSoup
 import json
 import sys
 
-args = sys.argv  # 引数を取得
-
+# args = sys.argv  # 引数を取得
+args = ["", "MS"]
 dataArray = []
-
 json_open = open('./test.json', 'r')
 timeTableData = json.load(json_open)
 
@@ -17,19 +17,23 @@ for i in range(num):
     val = timeTableData[i]  # 時間割コード取得
     res = requests.get(
         'https://kyo-web.teu.ac.jp/syllabus/2022/' + args[1] + '_' + val + '_ja_JP.html')
+    print(i)
+    if res.status_code == 404:
+        continue
     bs = BeautifulSoup(res.content, 'html.parser')
-    print(val)
+    table_elements = bs.find_all('table', class_='syllabus-normal')[0]
+    titles = table_elements.find_all('th', class_='syllabus-prin')
+    tds = table_elements.find_all('td')
 
-table_elements = bs.find_all('table', class_='syllabus-normal')[0]
-titles = table_elements.find_all('th', class_='syllabus-prin')
-tds = table_elements.find_all('td')
-
-for td in tds:
     ary = []
-    ary.append(td.get_text().strip().replace(
-        ' ', '').replace('\r\n', '').replace('\u30001', '').replace('\u3000', ''))
+
     for td in tds:
-        dataArray.append(ary)
+        ary.append(td.get_text().strip().replace(' ', '').replace(
+            '\r\n', '').replace('\u30001', '').replace('\u3000', ''))
+
+    dataArray.append(ary)
+
+    print(dataArray)
 
 with open('data.json', 'w') as f:
-    json.dump(array, f, ensure_ascii=False, indent=4)
+    json.dump(dataArray, f, ensure_ascii=False, indent=4)
