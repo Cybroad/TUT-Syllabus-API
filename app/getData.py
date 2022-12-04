e3787577f00b7eff2c7eaf3002718763a065238c
6mile <me@6mile.dev>
2022-12-05 08:48:14 +0900

    [Update] Docker化

--- app/getData.py
+++ app/getData.py
@@ -6,6 +6,8 @@
 # ===========================================================================
 # 時間割コード取得関数
 # ===========================================================================
+from bs4 import BeautifulSoup
+import requests
 import settings
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
@@ -15,30 +17,37 @@ import re
 import json
 
 options = Options()
-options.headless = True # Headlessモードで起動
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+
 
 def getcode(v):
     def getPageNum():  # 現在のページ数及び全体ページ数取得関数
         mPageN_o = driver.find_element(By.XPATH,  # エレメント抽出
-            '/html/body/form/div[2]/p[1]').text
+                                       '/html/body/form/div[2]/p[1]').text
         conditons = r'\全部で .*\あります'  # 任意の条件を設定
         mPageN_a = re.findall(conditons, mPageN_o)  # 条件にマッチする文字列を取得
         mPageN = int(mPageN_a[0].replace(
             '全部で ', '').replace('件あります', ''))  # 全体ページ数
         cPageN_o = driver.find_element(By.XPATH,  # エレメント抽出
-            '/html/body/form/div[2]/p[1]/b[2]').text
+                                       '/html/body/form/div[2]/p[1]/b[2]').text
         cPageN = int(cPageN_o.replace("件目", ''))  # 現在のページ数
 
         return cPageN, mPageN  # p[0]:現在のページ数, p[1]:全体ページ数
 
     # =========================================================================
 
-    driver = webdriver.Chrome(settings.chromeDriverPath, options=options)
+    driver = webdriver.Remote(
+        command_executor="http://selenium:4444/wd/hub",
+        options=options
+    )
     array = []
     driver.get("https://kyo-web.teu.ac.jp/campussy/")  # アクセスするURL
 
-    driver.switch_to.frame(driver.find_element(By.NAME, "search"))  # 検索フレームを切り替える
-    
+    driver.switch_to.frame(driver.find_element(
+        By.NAME, "search"))  # 検索フレームを切り替える
+
     print("...取得中")
     Select(driver.find_element(By.ID, 'jikanwariShozokuCode')
            ).select_by_value(v)  # コンピュータサイエンス学部
@@ -47,7 +56,7 @@ def getcode(v):
            ).select_by_value('200')  # 一覧表示件数
 
     driver.find_elements(By.XPATH,
-        '//*[@id = "jikanwariKeywordForm"]/table[2]/tbody/tr/td/table/tbody/tr[9]/td/input[1]')[0].click()  # 検索ボタンをクリック
+                         '//*[@id = "jikanwariKeywordForm"]/table[2]/tbody/tr/td/table/tbody/tr[9]/td/input[1]')[0].click()  # 検索ボタンをクリック
 
     driver.switch_to.default_content()  # 検索フレームをもとに戻す
 
@@ -55,20 +64,20 @@ def getcode(v):
         driver.find_element(By.NAME, "result"))  # 検索結果フレームを切り替える
 
     thNum = len(driver.find_elements(By.XPATH,
-        '/html/body/form/div[2]/table/tbody/tr'))  # エレメント数取得
+                                     '/html/body/form/div[2]/table/tbody/tr'))  # エレメント数取得
 
     for num in range(1, thNum):
         array.append(driver.find_element(By.XPATH,
-            '/html/body/form/div[2]/table/tbody/tr[' + str(num) + ']/td[4]').text)
+                                         '/html/body/form/div[2]/table/tbody/tr[' + str(num) + ']/td[4]').text)
 
     p = getPageNum()  # 現在のページ数及び全体ページ数取得関数を実行
     aTagN = int(len(driver.find_elements(By.XPATH,
-        '/html/body/form/div[2]/p[1]/a'))) - 1
+                                         '/html/body/form/div[2]/p[1]/a'))) - 1
 
-    if(aTagN > 0):
+    if (aTagN > 0):
         driver.find_elements(By.XPATH,
-            '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
-        
+                             '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
+
         while p[0] < p[1]:  # 現在のページ数が全体ページ数より小さい間
 
             p = getPageNum()  # 現在のページ数及び全体ページ数取得関数を実行
@@ -79,21 +88,21 @@ def getcode(v):
                 driver.find_element(By.NAME, "result"))  # 検索結果フレームを切り替える
 
             thNum = len(driver.find_elements(By.XPATH,
-                '/html/body/form/div[2]/table/tbody/tr'))  # エレメント数取得
+                                             '/html/body/form/div[2]/table/tbody/tr'))  # エレメント数取得
 
             for num in range(1, thNum):
                 array.append(driver.find_element(By.XPATH,
-                    '/html/body/form/div[2]/table/tbody/tr[' + str(num) + ']/td[4]').text)
+                                                 '/html/body/form/div[2]/table/tbody/tr[' + str(num) + ']/td[4]').text)
 
             aTagN = len(driver.find_elements(By.XPATH,
-                '/html/body/form/div[2]/p[1]/a')) - 1
+                                             '/html/body/form/div[2]/p[1]/a')) - 1
 
             driver.find_elements(By.XPATH,
-                '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
+                                 '/html/body/form/div[2]/p[1]/a')[aTagN].click()  # 次ページをクリック
 
     driver.quit()
 
-    with open(settings.timeTableCodeDir + "/" + v +'.json', 'w') as f:
+    with open(settings.timeTableCodeDir + "/" + v + '.json', 'w') as f:
         json.dump(array, f, ensure_ascii=False, indent=4)
     return "finish"
 
@@ -102,13 +111,10 @@ def getcode(v):
 # 講義データ取得関数 (時間割コード => 授業内容等(単位数))
 # ===========================================================================
 
-import requests
-from bs4 import BeautifulSoup
-import json
 
 def get_timetable(v):
     dataArray = []
-    json_open = open(settings.timeTableCodeDir + "/" + v +'.json', 'r')
+    json_open = open(settings.timeTableCodeDir + "/" + v + '.json', 'r')
     timeTableData = json.load(json_open)
 
     num = len(timeTableData)
@@ -131,5 +137,5 @@ def get_timetable(v):
 
         dataArray.append(ary)
 
-    with open(settings.lectureDataDir + "/" + v +'.json', 'w') as f:
+    with open(settings.lectureDataDir + "/" + v + '.json', 'w') as f:
         json.dump(dataArray, f, ensure_ascii=False, indent=4)
--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 # ====================================================
-# 東京工科大学 単位計算API
+# 東京工科大学 学内シラバス API
 #
 # 開発元：TUT22 Dev Team (tut22_dev@6mile.dev)
 # ====================================================
-
+import getData
 import settings
 from fastapi import FastAPI
 import json
+import uvicorn
 
 dataLists = []
 app = FastAPI()
+
+for i in settings.department:
+    print("> " + i + " 学部の時間割コードの取得を開始します")
+    x = getData.getcode(i)
+    if x == "finish":
+        print("> " + i + " 学部の時間割コードの取得が完了しました")
+        print("> " + i + " 学部の講義データの取得を開始します")
+        getData.get_timetable(i)
+        print("> " + i + " 学部の講義データの取得が完了しました")
+
+print("> 指定されたすべての学部のデータ取得が完了しました")
+
 
 def serchIndex(l, x, default=False):
     if x in l:
@@ -24,14 +37,17 @@ def serchIndexCheck(l, x):
     else:
         return False
 
+
 print("> " + str(len(settings.department)) + "個の学部別シラバスデータの取得を開始します")
 
 for i in range(len(settings.department)):
-    readFile = open(settings.lectureDataDir + "/" + settings.department[i] + '.json', 'r')
+    readFile = open(settings.lectureDataDir + "/" +
+                    settings.department[i] + '.json', 'r')
     for val in json.load(readFile):
         dataLists.append(val)
 
 print("> シラバスデータ取得完了")
+
 
 @app.get("/debug/{id}")
 def getTimeTableInfo(id: str):
@@ -42,7 +58,7 @@ def getTimeTableInfo(id: str):
 
 
 @app.post("/getInfoAll/")
-def getTimeTableInfo(data:list):
+def getTimeTableInfo(data: list):
     ary = 0
     print(data)
     for val in data:
@@ -52,3 +68,7 @@ def getTimeTableInfo(data:list):
                     ary += int(dataLists[i][9])
 
     return ary
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
