49e99a53fa4aa189f1df985f18ea0e0c5e03b1e9
6mile <me@6mile.dev>
2022-12-05 14:45:26 +0900

    [Update] 取得頻度の改善

--- app/main.py
+++ app/main.py
@@ -12,16 +12,19 @@ import uvicorn
 dataLists = []
 app = FastAPI()
 
-for i in settings.department:
-    print("> " + i + " 学部の時間割コードの取得を開始します")
-    x = getData.getcode(i)
-    if x == "finish":
-        print("> " + i + " 学部の時間割コードの取得が完了しました")
-        print("> " + i + " 学部の講義データの取得を開始します")
-        getData.get_timetable(i)
-        print("> " + i + " 学部の講義データの取得が完了しました")
+print(os.path.exists("/app/timeTableId_Data")
+      and os.path.exists("/app/lecture_Data"))
+if (not (os.path.exists(settings.timeTableCodeDir) and os.path.exists(settings.lectureDataDir))):
+    for i in settings.department:
+        print("> " + i + " 学部の時間割コードの取得を開始します")
+        x = getData.getcode(i)
+        if x == "finish":
+            print("> " + i + " 学部の時間割コードの取得が完了しました")
+            print("> " + i + " 学部の講義データの取得を開始します")
+            getData.get_timetable(i)
+            print("> " + i + " 学部の講義データの取得が完了しました")
 
-print("> 指定されたすべての学部のデータ取得が完了しました")
+    print("> 指定されたすべての学部のデータ取得が完了しました")
 
 
 def serchIndex(l, x, default=False):
