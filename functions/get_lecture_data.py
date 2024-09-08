f9e2690f3ae504279ee1b3c7256be3c15a951010
kakeru hokida <me@6mile.dev>
2024-09-08 13:13:20 +0900

    授業情報ページ内のすべての情報を取得できるよう修正

--- functions/get_lecture_data.py
+++ functions/get_lecture_data.py
@@ -1,13 +1,13 @@
-from bs4 import BeautifulSoup
+import ssl
+from typing import Any
+from bs4 import BeautifulSoup, ResultSet
 import requests
-import re
 import datetime
+import urllib3
 
-# ===========================================================================
-# 講義データ取得関数 (時間割コード => 授業内容等(単位数))
-# ===========================================================================
+TUT_SYLLABUS_URL = 'https://kyo-web.teu.ac.jp/syllabus'
 
-class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+class _CustomHttpAdapter (requests.adapters.HTTPAdapter):
     def __init__(self, ssl_context=None, **kwargs):
         self.ssl_context = ssl_context
         super().__init__(**kwargs)
@@ -17,31 +17,48 @@ class CustomHttpAdapter (requests.adapters.HTTPAdapter):
             num_pools=connections, maxsize=maxsize,
             block=block, ssl_context=self.ssl_context)
 
-def fetch_syllabus(url: str):
+def _fetch_syllabus(current_year: int, department_name: str, lecture_code: str):
     session = requests.session()
     ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     ctx.options |= 0x4
-    session.mount('https://', CustomHttpAdapter(ctx))
-    res = session.get(url)
+    session.mount('https://', _CustomHttpAdapter(ctx))
+    res = session.get(f'{TUT_SYLLABUS_URL}/{current_year}/{department_name}_{lecture_code}_ja_JP.html')
     return res
 
-def get_timetable(lecture_code : str, department_name : str):
-    now_year = datetime.datetime.now().year
-    print("...取得中")
-        print(val)
+def _text_fotmat(text: str) -> str:
+    return text.strip().replace(' ', '').replace(
+        '\r\n', '').replace('\u30001', '').replace('\u3000', '').replace('\n', '').replace('\r', '').replace('\t', '')
 
-    res = fetch_syllabus(f'https://kyo-web.teu.ac.jp/syllabus/{now_year}/{department_name}_{lecture_code}_ja_JP.html')
+
+def _join_lecture_information(lecture_informations: ResultSet[Any]) -> list[str]:
+    joined_lecture_information_list = []
+
+    for lecture_information in lecture_informations:
+        td_data = lecture_information.find_all('td')
+
+        for td in td_data:
+            joined_lecture_information_list.append(_text_fotmat(td.text))
+
+    return joined_lecture_information_list
+
+# ===========================================================================
+# 講義データ取得関数 (時間割コード => 授業内容等(単位数))
+# ===========================================================================
+def get_timetable(department_name: str, lecture_code : str,):
+    now_year = datetime.datetime.now().year
+    res = _fetch_syllabus(now_year, department_name, lecture_code)
 
     if res.status_code == 404:
-        continue
+        return None
+
     bs = BeautifulSoup(res.content, 'html.parser')
-    table_elements = bs.find_all('table', class_='syllabus-normal')[0]
-    tds = table_elements.find_all('td')
 
-    ary = {}
+    tab_elements = bs.find_all('table', class_='syllabus-normal')
 
-    for td in tds:
-        ary.append(td.get_text().strip().replace(' ', '').replace(
-            '\r\n', '').replace('\u30001', '').replace('\u3000', ''))
+    # 講義情報を格納するリスト
+    joined_lists = _join_lecture_information(tab_elements)
+
+    print(joined_lists)
 
 
+get_timetable('CS','11041C1')
