0e5017796ae3a9e2bcafcbaf8cae566a413c980c
kakeru hokida <me@6mile.dev>
2024-09-08 16:30:10 +0900

    コンソール出力を追加

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ os.makedirs("docs/api/v1", exist_ok=True)
 
 for dept in department:
     os.makedirs(f"docs/api/v1/{dept}", exist_ok=True)
+    print(f"Getting {dept} lecture codes...")
     # 指定学部の講義コードを取得
     lecture_codes = get_lecture_code.get_lecture_code(dept)
+    print(f"Got {len(lecture_codes)} lecture codes.")
     for lecture_code in lecture_codes:
         # 講義データを取得
-        sleep(1)
+        print(f"Getting {dept}/{lecture_code}...")
         lecture_data = get_lecture_data.get_timetable(dept, lecture_code)
         if lecture_data:
             with open(f"docs/api/v1/{dept}/{lecture_code}.json", "w") as f:
                 json.dump(lecture_data, f, ensure_ascii=False, indent=4)
+                print(f"Saved {dept}/{lecture_code}.json")
